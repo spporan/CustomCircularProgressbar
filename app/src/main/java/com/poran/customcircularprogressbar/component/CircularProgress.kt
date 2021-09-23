356f319485b648c56e21023ac8f64d9c2b7bfae8
ridmik <poran.cse@gmail.com>
2021-09-23 20:24:15 +0600

    created CircularProgress.kt to implement functionality

---
+++ app/src/main/java/com/poran/customcircularprogressbar/component/CircularProgress.kt
@@ -0,0 +1,12 @@
+package com.poran.customcircularprogressbar.component
+
+import android.content.Context
+import android.util.AttributeSet
+
+class CircularProgress @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) {
+
+}
