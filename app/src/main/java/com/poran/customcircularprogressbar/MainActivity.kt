40f68c82310236c24f342d60c3b30c5091ef8792
ridmik <poran.cse@gmail.com>
2021-09-23 23:25:43 +0600

    implemented CircularProgress.kt draw and animation, calculation

--- app/src/main/java/com/poran/customcircularprogressbar/MainActivity.kt
+++ app/src/main/java/com/poran/customcircularprogressbar/MainActivity.kt
@@ -2,10 +2,22 @@ package com.poran.customcircularprogressbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.poran.customcircularprogressbar.component.CircularProgress
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var default = 60;
+
+        val progressView = findViewById<CircularProgress>(R.id.progress_circular)
+        progressView.progressValue = default
+
+        findViewById<Button>(R.id.progress_btn).setOnClickListener {
+            default += 4
+            if (default > 100) return@setOnClickListener
+            progressView.progressValue = default
+        }
     }
 }
--- app/src/main/java/com/poran/customcircularprogressbar/component/CircularProgress.kt
+++ app/src/main/java/com/poran/customcircularprogressbar/component/CircularProgress.kt
@@ -1,33 +1,54 @@
 package com.poran.customcircularprogressbar.component
 
+import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Typeface
+import android.content.res.Resources
+import android.graphics.*
 import android.util.AttributeSet
+import android.view.View
+import android.view.animation.DecelerateInterpolator
 import com.poran.customcircularprogressbar.R
+import kotlin.math.min
 
 class CircularProgress @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) {
+): View(context, attrs, defStyleAttr) {
     private val defaultBackgroundColor = Color.GRAY
     private val defaultProgressColor = Color.GREEN
-    private var strokedWidth: Float = 0f
-    private var progressPaint: Paint
-    private var backgroundProgressPaint: Paint
-    private var textProgressPaint: Paint
-    var progressValue: Int
-    companion object {
-        const val DEFAULT_STROKE_WIDTH = 5f
-    }
+    private val dpToPx = Resources.getSystem().displayMetrics.density
+    private val defaultStrokeWidth = 16 * dpToPx
+    private var strokedWidth: Float = defaultStrokeWidth
+    private val progressPaint: Paint
+    private val backgroundProgressPaint: Paint
+    private val textProgressPaint: Paint
+    private lateinit var progressBounds: RectF
+    private val animationInterpolator by lazy { DecelerateInterpolator() }
+
+    private var sweepAngle: Float = 250f
+    private var centerX : Float = 0f
+    private var centerY : Float = 0f
+    var progressValue: Int = 0
+        set(value) {
+            field = value
+            ValueAnimator.ofFloat(sweepAngle, 360f / 100f * progressValue).apply {
+                interpolator = animationInterpolator
+                duration = 300
+                addUpdateListener { animation ->
+                    sweepAngle = animation.animatedValue as Float
+                    invalidate()
+                }
+                start()
+            }
+        }
+
     init {
-        val  typedArray = context.obtainStyledAttributes(attrs, R.styleable.Progress)
-        val backgroundColor = typedArray.getColor(R.styleable.Progress_colorBackground, defaultBackgroundColor)
-        val progressColor = typedArray.getColor(R.styleable.Progress_colorProgress, defaultProgressColor)
-        val defaultProgress = typedArray.getInteger(R.styleable.Progress_defaultProgressValue, 0)
-        strokedWidth = typedArray.getDimension(R.styleable.Progress_strokeWidth, DEFAULT_STROKE_WIDTH)
+        val  typedArray = context.obtainStyledAttributes(attrs, R.styleable.CircularProgress)
+        val backgroundColor = typedArray.getColor(R.styleable.CircularProgress_colorBackground, defaultBackgroundColor)
+        val progressColor = typedArray.getColor(R.styleable.CircularProgress_colorProgress, defaultProgressColor)
+        val defaultProgress = typedArray.getInteger(R.styleable.CircularProgress_defaultProgressValue, 0)
+        strokedWidth = typedArray.getDimension(R.styleable.CircularProgress_strokeWidth, defaultStrokeWidth)
 
         // paint tools for paint circular progress
         progressPaint = Paint().apply {
@@ -38,6 +59,7 @@ class CircularProgress @JvmOverloads constructor(
             strokeWidth = strokedWidth
         }
 
+        // paint background circle
         backgroundProgressPaint = Paint().apply {
             isAntiAlias = true
             color = backgroundColor
@@ -45,7 +67,7 @@ class CircularProgress @JvmOverloads constructor(
             strokeCap = Paint.Cap.ROUND
             strokeWidth = strokedWidth
         }
-
+        // paint progress text value
         textProgressPaint = Paint().apply {
             isAntiAlias = true
             color = Color.BLACK
@@ -54,8 +76,34 @@ class CircularProgress @JvmOverloads constructor(
             typeface = Typeface.create("cabin", Typeface.BOLD)
             textSize = 100f
         }
+
         progressValue = defaultProgress
         typedArray.recycle()
     }
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+
+        val radius = (min(width, height)/2.0 * 0.7).toFloat()
+        val viewWidth = layoutParams.width.toFloat()
+        val viewHeight = layoutParams.height.toFloat()
+
+        val arcStart = strokedWidth + 50
+        val arcEnd = viewWidth - arcStart
+        progressBounds = RectF(arcStart, arcStart, arcEnd, arcEnd)
+
+        centerX = viewWidth/ 2f
+        centerY = viewHeight / 2f
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.drawArc(progressBounds, 90f, 360f, false, backgroundProgressPaint)
+
+        canvas?.drawArc(progressBounds, 90f, sweepAngle, false, progressPaint)
+
+        canvas?.drawText("$progressValue%", centerX, centerY + 35f, textProgressPaint)
+    }
+
 
 }
